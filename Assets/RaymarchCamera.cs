eeef003bd7f3d4f09eb735fb96cf4914687e44fb
Sennettming <36041467+Sennettming@users.noreply.github.com>
2020-06-22 19:07:08 +0800

    Initial commit

---
+++ Assets/RaymarchCamera.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public class RaymarchCamera : MonoBehaviour
+{
+    [SerializeField]// see in inspector for private variables
+    private Shader _shader;
+
+    public Material _rayMarchMaterial
+    {
+        get
+        { 
+            if (!_raymarchMat && _shader)
+            {
+                _raymarchMat = new Material(_shader);
+                _raymarchMat.hideFlags = HideFlags.HideAndDontSave; // not be disposed
+            }
+            return _raymarchMat;
+        }
+    }
+
+    private Material _raymarchMat;
+
+    public Camera _camera
+    {
+        get
+        {
+            if (!_cam)
+            {
+                _cam = GetComponent<Camera>();
+            }
+            return _cam;
+        }
+    }
+    
+    private Camera _cam;
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)// communicate with shader
+    {
+        if (!_rayMarchMaterial)
+        {
+            Graphics.Blit(src, dest);
+            return;
+        }
+        _rayMarchMaterial.SetMatrix("_CamFrustum", CamFrustum(_camera));
+        _rayMarchMaterial.SetMatrix("_CamToWorld", _camera.cameraToWorldMatrix);
+        _rayMarchMaterial.SetVector("_CamWorldSpace", _camera.transform.position);
+
+        RenderTexture.active = dest;
+        GL.PushMatrix();
+        GL.LoadOrtho();
+        _rayMarchMaterial.SetPass(0);
+        GL.Begin(GL.QUADS);
+        
+        //BL
+        GL.MultiTexCoord2(0, 0.0f, 0.0f);
+        GL.Vertex3(0.0f, 0.0f, 3.0f);  
+        //BR
+        GL.MultiTexCoord2(0, 1.0f, 0.0f);
+        GL.Vertex3(1.0f, 0.0f, 2.0f);  
+        //TR
+        GL.MultiTexCoord2(0, 1.0f, 1.0f);
+        GL.Vertex3(1.0f, 1.0f, 1.0f);  
+        //TL
+        GL.MultiTexCoord2(0, 0.0f, 1.0f);
+        GL.Vertex3(0.0f, 1.0f, 0.0f);
+        
+        GL.End();
+        GL.PopMatrix();
+    }
+
+    private Matrix4x4 CamFrustum(Camera cam)
+    {
+        Matrix4x4 frustum = Matrix4x4.identity;
+        float fov = Mathf.Tan((cam.fieldOfView * 0.5f) * Mathf.Deg2Rad);
+
+        Vector3 goUp = Vector3.up * fov;
+        Vector3 goRight = Vector3.right * fov * cam.aspect;
+
+        Vector3 TL = (-Vector3.forward - goRight + goUp);
+        Vector3 TR = (-Vector3.forward + goRight + goUp);
+        Vector3 BR = (-Vector3.forward + goRight - goUp);
+        Vector3 BL = (-Vector3.forward - goRight - goUp);
+        
+        frustum.SetRow(0, TL);
+        frustum.SetRow(1, TR);
+        frustum.SetRow(2, BR);
+        frustum.SetRow(3, BL);
+
+        return frustum;
+    }
+}
---
+++ Assets/RaymarchingSphere.shader
@@ -0,0 +1,66 @@
+﻿Shader "PeerPlay/RaymarchingSphere"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma target 3.0
+
+            #include "UnityCG.cginc"
+
+            sampler2D _MainTex;
+            uniform float4 _CamWorldSpace;
+            uniform float4x4 _CamFrustum;
+            uniform float4x4 _CamToWorld;
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+                float3 ray : TEXCOORD1;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                half index = v.vertex.z;
+                v.vertex.z = 0;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+
+                o.ray = _CamFrustum[(int)index].xyz;
+
+                o.ray /= abs(o.ray.z);
+                o.ray = mul(_CamToWorld, o.ray);
+                return o;
+            }
+
+
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // return float4(1,1,1,1);
+                float3 rayDir = normalize(i.ray.xyz);
+                float3 rayOrigin = _CamWorldSpace;
+                return fixed4(rayDir, 1);
+            }
+            ENDCG
+        }
+    }
+}
